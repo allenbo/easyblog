365bf8f39c4f9b3d83926106d391422515534c3c
abo <linjianfengqrh@gmail.com>
2014-01-16 23:21:56 +0800

    Keep adding style sheet

--- blog/models.py
+++ blog/models.py
@@ -2,13 +2,19 @@ from django.db import models
 
 # Create your models here.
 
+
+class Category(models.Model):
+  name = models.CharField(max_length=100)
+
+
+
 class Post(models.Model):
   title = models.CharField(max_length=100)
   slug = models.SlugField(max_length = 100)
   content = models.TextField()
   published = models.BooleanField(default = False)
   created_date = models.DateTimeField(auto_now_add = True)
-
+  category = models.ManyToManyField(Category, blank=True)
 
 
   class Meta:
@@ -19,3 +25,17 @@ class Post(models.Model):
 
   def get_abs_url(self):
     return 'blog/%s' % self.id
+
+  def get_visit_num(self):
+    return 0
+
+  def get_reply_num(self):
+    return 0
+
+
+
+class Reply(models.Model):
+  name = models.CharField(max_length=100)
+  email = models.EmailField(blank = True)
+  to = models.IntegerField(default=0)
+  post = models.ForeignKey(Post)
