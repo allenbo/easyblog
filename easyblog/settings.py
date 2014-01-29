76f55c26bd8c19ff0ef78db7a6414d25698f5f2a
abo <linjianfengqrh@gmail.com>
2014-01-29 08:00:52 -0500

    A better sidebar

--- easyblog/settings.py
+++ easyblog/settings.py
@@ -67,6 +67,13 @@ TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, 'templates'),
 )
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.core.context_processors.static',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.request',
+)
 
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
